317cc60e5e3af919c000b1930c190b7a11f3a819
austinxduong <axduong2@gmail.com>
2021-06-11 15:40:41 -0700

    fixed postgreSQL connection

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,21 +2,26 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+// import Dog from '../lib/models/Dogs.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+
+  it('test communicating to POST method in controller', async () => {
+    const res = await request(app)
+      .post('/api/v1/dogs')
+      .send({ name: 'cliffy', age: 2, weight: '2 lbs' });
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'cliffy',
+      age: '2',
+      weight: '2 lbs'
+    });
+  });
+
 });
 
-it('test communicating to POST method in controller', async () => {
-  const respond = await request(app)
-    .post('/api/v1/dogs')
-    .send({ name: 'cliffy', age: 2, weight: '2 lbs' });
-  expect(respond.body).toEqual({
-    id: '1',
-    name: 'cliffy',
-    age: '2',
-    weight: '2 lbs'
-  });
-});
