c94deab36d85dd6d22b0654e0918bd9dcfdcca8c
austinxduong <axduong2@gmail.com>
2021-06-13 15:31:53 -0700

    still trying to pass first test for .post route controller/model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,24 +4,33 @@ import request from 'supertest';
 import app from '../lib/app.js';
 // import Dog from '../lib/models/Dogs.js';
 
-describe('demo routes', () => {
+describe('dog routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
+  // CRUD
+  // C - create POST      INSERT
+  // R - read   GET       SELECT
+  // U - update PUT       UPDATE
+  // D - delete DELETE    DELETE
 
-  it('test communicating to POST method in controller', async () => {
+  it('creates a dog via POST', async () => {
     const res = await request(app)
       .post('/api/v1/dogs')
-      .send({ name: 'cliffy', age: 2, weight: '2 lbs' });
+      .send({ 
+        name: 'cliffy', 
+        age: 2, 
+        weight: '2 lbs' 
+      });
 
     expect(res.body).toEqual({
       id: '1',
       name: 'cliffy',
-      age: '2',
+      age: 2,
       weight: '2 lbs'
     });
   });
-
 });
+
 
