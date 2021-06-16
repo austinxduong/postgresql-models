30dde34079ce0461d046aa1336cb71ebf98c25c2
austinxduong <axduong2@gmail.com>
2021-06-16 09:10:38 -0700

    still trying to pass the .post test for pizza model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,17 @@
-DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS dogs, pizza;
 
 CREATE TABLE dogs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
     age INTEGER NOT NULL,
     weight TEXT
+);
+
+-- does creating the table help pass test for .post for pizza model?
+
+CREATE TABLE pizza (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    topping TEXT,
+    style TEXT
 );
